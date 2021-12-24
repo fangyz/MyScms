bf369e5ef8ae3d8ee4f9c839237ab93e1fdee8d1
fangyz <2904190811@qq.com>
2021-12-24 19:09:26 +0800

    测试grpc

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GreeterClientTest.GreeterClientTestClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "测试我的grpc~" });
+            Console.WriteLine("Greeting回复: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-这是我以学习为目的做的供应链管理系统。打算包括物料管理、工单管理、请购、采购、仓库等功能，仍在持续完善中。
+这是我以学习为目的做的供应链管理系统，仍在不断跟新中。
 
-技术上采用的是asp.net core mvc、ef core、vue、bootstrap
+技术上采用的是asp.net core mvc、webapi、ef core、vue、bootstrap
